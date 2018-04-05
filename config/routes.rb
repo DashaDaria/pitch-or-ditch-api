c50abea7619cef789e72861aa637bda30760bbfe
DashaDaria <dashadariallc@gmail.com>
2018-04-04 23:54:32 -0600

    added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+namespace :api do
+  resources :ideas, except: [:new, :edit]
+end
+
+namespace :api do
+  resources :categories, only: [:index, :show] do
+    resources :ideas, only: [:show]
+  end
+end
+
 end
