b3066abd1bb98eb2015216d5f99b609c8d444731
DashaDaria <dashadariallc@gmail.com>
2018-04-05 14:23:41 -0600

    added a create action for ideas controller

--- app/controllers/api/ideas_controller.rb
+++ app/controllers/api/ideas_controller.rb
@@ -1,5 +1,21 @@
 class Api::IdeasController < ApplicationController
+
   def index
     render json: Idea.all
   end
+
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      render json: idea
+    else
+      render json: {message: idea.errors}, status: 400
+    end
+  end
+
+  private
+  def idea_params
+    params.require(:idea).permit(:category_id, :name, :content, :author)
+  end
+
 end
