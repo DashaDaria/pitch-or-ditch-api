5381023e75ab64cc73d95198ed849d061ded1628
DashaDaria <dashadariallc@gmail.com>
2018-04-09 15:43:11 -0600

    updated strong params for update action

--- app/controllers/api/ideas_controller.rb
+++ app/controllers/api/ideas_controller.rb
@@ -27,7 +27,7 @@ class Api::IdeasController < ApplicationController
 
   private
   def idea_params
-    params.require(:idea).permit(:category_id, :name, :content, :author)
+    params.require(:idea).permit(:category_id, :name, :content, :author, :votes)
   end
 
   def set_idea
